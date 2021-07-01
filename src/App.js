b3c14a4a3a1031fe12769b3c304aaea5595c2680
vickyocampoo <vicky.a.ocampo@gmail.com>
2021-07-01 15:08:14 -0300

    test andando

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-         comprar 
+         Seguinos 
          <img src={logoig} className="App-link img" alt=" " /> 
         </a>
       </header>
