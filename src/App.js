0f47e566966f844854b374ff4a0d6550cf009b00
vickyocampoo <vicky.a.ocampo@gmail.com>
2021-07-01 20:28:02 -0300

    error test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-         Seguinos
+         Parcial IS
          <img src={logoig} className="App-link img" alt=" " /> 
         </a>
       </header>
