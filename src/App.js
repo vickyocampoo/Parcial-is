1a73db30a0cbd20b46da247ad8200305a2d3a508
vickyocampoo <vicky.a.ocampo@gmail.com>
2021-07-01 19:10:09 -0300

    test ok

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         > 
-         parcial 3 is
+         Seguinos
          <img src={logoig} className="App-link img" alt=" " /> 
         </a>
       </header>
