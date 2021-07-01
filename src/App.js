740d92e5ac8a26534016f9c28b12535362d35f3b
vickyocampoo <vicky.a.ocampo@gmail.com>
2021-07-01 01:01:18 -0300

    cambios con error de test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import logo from './logo.jpeg';
+import logoig from './logoig.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+         <img src={logo} alt=" " />
         <p>
-         Hola soy Vicky
+         Hola, somos Rotoria Indumentaria. En nuestro perfil 
+         podes encontrar ropa deportiva de hombre y de mujer.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/rotoria_indumentaria/"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > 
+         Hola soy Vicky
+         <img src={logoig} className="App-link img" alt=" " /> 
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Seguinos link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Seguinos/i);
   expect(linkElement).toBeInTheDocument();
 });
